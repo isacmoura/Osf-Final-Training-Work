5e13c51fcd751a5c5f14856bd89d78aa8a235a56
Isac Moura Gomes <contatoisacmoura@outlook.com>
2018-11-22 11:16:17 -0300

    List function

--- index.js
+++ index.js
@@ -2,3 +2,27 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var request = require('request');
 
+var app = express();
+var api = 'https://cryptic-retreat-41638.herokuapp.com/api/contacts';
+
+app.set("views", __dirname + "/view");
+app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// ROUTES
+app.get('/list', function (req, res) {
+    request.get(api, function (error, response, body) {
+        console.log(body)
+        res.render('list', {
+            contacts: JSON.parse(body)
+        });
+    });
+});
+
+app.get('/register', function (req, res) {
+    
+});
+
+app.listen(3030, function() {
+    console.log('Running on the port 3030');
+});
