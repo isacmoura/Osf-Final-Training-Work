e471238886a29cfd59fd87e91fc0ba5c9f34e81a
Isac Moura Gomes <contatoisacmoura@outlook.com>
2018-11-22 14:56:19 -0300

    Question 2

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var request = require('request');
+var path = require('path');
 
 var app = express();
 var api = 'https://cryptic-retreat-41638.herokuapp.com/api/contacts';
@@ -8,6 +9,9 @@ var api = 'https://cryptic-retreat-41638.herokuapp.com/api/contacts';
 app.set("views", __dirname + "/view");
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(express.static('public'));
+
 
 // ROUTES
 app.get('/list', function (req, res) {
@@ -20,7 +24,31 @@ app.get('/list', function (req, res) {
 });
 
 app.get('/register', function (req, res) {
-    
+    res.render('register');
+});
+
+
+
+app.post('/register', function (req, res) {
+    var user = {
+        "name" : req.body.name,
+        "email" : req.body.email,
+        "gender" : req.body.gender,
+        "phone" : req.body.phone
+    };
+
+    request.post({
+        url: api, 
+        headers: {'content-type': 'application/x-www-form-urlencoded'},
+        body: require('querystring').stringify(user)
+    },
+
+    function (error, response, body) {
+        res.redirect('list');
+        console.log(body);
+    });
+
+
 });
 
 app.listen(3030, function() {
