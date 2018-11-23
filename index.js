b206c023eb443f1dbf48ab91199ff12db3be71b3
Isac Moura Gomes <contatoisacmoura@outlook.com>
2018-11-22 23:58:17 -0300

    Final project

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ var request = require('request');
 var path = require('path');
 
 var app = express();
-var api = 'https://cryptic-retreat-41638.herokuapp.com/api/contacts';
+var api = "https://f5zg6v0z92.execute-api.us-east-1.amazonaws.com/dev/contacts/";
 
 app.set("views", __dirname + "/view");
 app.set("view engine", "ejs");
@@ -39,8 +39,8 @@ app.post('/register', function (req, res) {
 
     request.post({
         url: api, 
-        headers: {'content-type': 'application/x-www-form-urlencoded'},
-        body: require('querystring').stringify(user)
+        method: "POST",
+        json: user
     },
 
     function (error, response, body) {
@@ -58,13 +58,70 @@ app.get('/search', function(req, res) {
 app.get('/searchUser', function(req, res) {
     var id = req.query.id;
 
-    request.get(`https://f5zg6v0z92.execute-api.us-east-1.amazonaws.com/dev/contacts/${id}`, function (error, response, body) {
+    request.get(api+`${id}`, function (error, response, body) {
         
         res.render('show', {
             user: JSON.parse(body)
-        });
+        })
         console.log("Body" + body);
     });
+});
+
+app.get('/searchUser/:id', function(req, res) {
+    var id = req.params.id;
+
+    request.get(api+`${id}`, function (error, response, body) {
+        
+        res.render('show', {
+            user: JSON.parse(body)
+        })
+        console.log("Body" + body);
+    });
+});
+
+app.get('/update/:id', function (req, res) {
+    var id = req.params.id;
+
+    request.get('https://f5zg6v0z92.execute-api.us-east-1.amazonaws.com/dev/contacts/' + id, function (error, response, body) {
+        res.render('update', {
+            user: JSON.parse(body)
+        })
+    })
+});
+
+app.post('/update', function (req, res) {
+    
+    var user = {
+        "name" : req.body.name,
+        "email" : req.body.email,
+        "gender" : req.body.gender,
+        "phone" : req.body.phone
+    }
+
+    request.put({
+        url: api + `${req.body.id}`,
+        method: "POST",
+        json: user
+    },
+
+    function (error, response, body){
+        res.redirect('/searchUser/' + req.body.id)
+    });
+    
+});
+
+app.get('/delete/:id', function (req, res) {
+    var id = req.params.id;
+
+    request.delete({
+        url: api + id,
+        method: "POST",
+        json: id
+    }, 
+    
+    function (error, response, body) {
+        res.redirect('/list')
+    })
 });
 
 app.listen(3030, function() {
